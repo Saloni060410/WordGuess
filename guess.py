369fb858a870c85f31fb97142b5ed91a4ab5c458
Saloni Sawant <salonisawant225010@gmail.com>
2025-11-30 14:21:10 +0530

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,37 @@
+import random
+
+wordBank=[ "planet", "orange", "guitar", "castle", "bridge", "forest", "frozen","golden", "shadow", "mirror", "socket", "switch", "window", "rocket","python", "banana", "market", "random", "wizard", "legend", "mystic","galaxy", "island", "beauty", "silver", "danger", "hunter", "puzzle","thunder", "crystal", "diamond", "journey", "kingdom", "network","firefly", "butter", "cookie", "candle", "tunnel", "marker", "circle","impact", "instead", "perfect", "victory", "freedom", "passion","horizon", "awesome", "gravity", "valley", "engine", "device", "system"]
+
+word=random.choice(wordBank)
+
+attempts=10
+
+current_w=["_"]*len(word)
+
+while attempts>0:
+
+    print("Current Word : ","".join(current_w))
+    guess=input("Guess a letter : ").lower()
+
+    if guess in word:
+        for i in range(len(word)):
+            if word[i]==guess:
+                current_w[i]=guess
+        print("Correct Guess")
+
+    else:
+        print("Wrong Guess")
+        attempts=attempts-1
+        print("Remaining attempts : ",attempts)
+    
+    if "_" not in current_w:
+        print("\nCongratulations you guessed the word.")
+        print("Word is : ","".join(current_w))
+        break
+    
+    if "_" in current_w and attempts==0:
+        print("You ran out of attempts...Better luck next time.")
+        break
+
+    print("\n")
+    
